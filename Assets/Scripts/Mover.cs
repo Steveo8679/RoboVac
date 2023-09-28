a8d27a38c6384c63f2ca6493e9b0e9cd588d3a0a
Stephen <Steve.J.Fitz@gmail.com>
2023-09-28 20:05:31 +1000

    Get cam working

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,19 +2,32 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float moveSpeed = 0.5f;
+    [SerializeField] float rotationSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // Assuming your character's forward direction is along the Z-axis.
+        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-		transform.Translate(xValue, 0, zValue);
-	}
+        // Check if there is any movement.
+        if (movementDirection != Vector3.zero)
+        {
+            // Calculate the rotation angle based on the movement direction.
+            Quaternion targetRotation = Quaternion.LookRotation(movementDirection.normalized);
+
+            // Smoothly rotate the character towards the target rotation.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            // Move the character forward.
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
 }
